d9594121890bb0949d7480c1c41f4c899d4ef1d1
imsamar <samar.joharchi@gmail.com>
2023-02-14 01:43:21 +0330

    added JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 alert("Happy Valentine's Day,be gentle with your skin...");
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Thuesday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#date");
+let date = new Date();
+dateElement.innerHTML = formatDate(date);
